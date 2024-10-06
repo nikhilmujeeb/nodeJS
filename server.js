0f73ac79aa920ead23d0b66a84682e319c694bca
nikhilmujeeb <nikhilmujeeb@gmail.com>
2024-10-06 23:11:31 +0530

    Initial commit

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Car Products API!');
+});
+
 let products = [
   {
     id : 1,
